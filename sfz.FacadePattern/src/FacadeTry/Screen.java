77e8d00caab542b04eea99693faf2f52cd0d8868
shen <1025695049@qq.com>
2020-11-06 18:36:06 +0800

    外观模式：控制某场景效果展示

---
+++ sfz.FacadePattern/src/FacadeTry/Screen.java
@@ -0,0 +1,20 @@
+package FacadeTry;
+
+/**
+ * @Author Shen Fangzhi
+ * @Description: This class represents the screen to display the video.
+ * @Date: Created in 17:29 2020/11/6
+ * @Modified By:
+ **/
+public class Screen {
+    public Screen() {
+    }
+
+    public void screenUp() {
+        System.out.println("The screen is lifting.");
+    }
+
+    public void screenDown() {
+        System.out.println("The screen is dropping.");
+    }
+}
