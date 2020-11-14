8a4d5e62dd8afbfe7cbb0a5a07dd77ffb0e246e7
tongjinyx <43092694+tongjinyx@users.noreply.github.com>
2020-11-14 09:59:27 +0800

    Add files via upload

---
+++ prototypepattern/Turtle.java
@@ -0,0 +1,32 @@
+package com.java.nyx.prototypepattern;
+
+/**
+ * @Author Nie Yixin
+ * @Description:
+ * @Date: Created in 10:12 2020/11/13
+ * @Modified By:
+ **/
+public class Turtle extends Fish{
+    public String color;
+    public String type;
+    public int weight;
+    public int year;
+
+    public Turtle() {
+
+    }
+    public Turtle (Turtle target) {
+        super(target);
+        if (target!=null) {
+            this.color = target.color;
+            this.type = target.type;
+            this.weight = target.weight;
+            this.year = target.year;
+        }
+    }
+
+    @Override
+    public Turtle clone() {
+        return new Turtle(this);
+    }
+}
