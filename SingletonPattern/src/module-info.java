0cb373d0203db5c4bf2ff21c042ab9231fff91dd
ccd233 <49812015+ccd233@users.noreply.github.com>
2020-11-17 21:21:58 +0800

    Add files via upload

---
+++ SingletonPattern/src/module-info.java
@@ -0,0 +1,4 @@
+module SingletonPattern1 {
+    requires tools;
+    exports singletonpattern;
+}
---
+++ SingletonPattern/src/singletonpattern/SingletonDemo.java
@@ -0,0 +1,39 @@
+package singletonpattern;
+import tools.Tool;
+
+/**
+ * @Author Chi Chengdao
+ * @Description: 单例模式测试demo，使用双线程进行测试
+ **/
+
+public class SingletonDemo {
+    public static void work() {
+        Tool.print("Singleton Test Demo");
+        System.out.println("If you see the same name(小鲤鱼泡泡), then singleton was reused " + "\n" +
+                "If you see different name(小鲤鱼泡泡,水母小美美), then 2 singletons were created " + "\n\n" +
+                "RESULT:" + "\n");
+        Thread thread1 = new Thread(new Thread1());
+        Thread thread2 = new Thread(new Thread2());
+        thread1.start();
+        thread2.start();
+    }
+
+    static class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("小鲤鱼泡泡");
+            System.out.println(singleton.name);
+        }
+    }
+
+    static class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            Singleton singleton = Singleton.getInstance("水母小美美");
+            System.out.println(singleton.name);
+        }
+    }
+    public static void main(String[] args) {
+        work();
+    }
+}
