46d1a16371ead48e877f76ba4412b65c1825c323
tongjinyx <43092694+tongjinyx@users.noreply.github.com>
2020-11-17 21:34:30 +0800

    Add files via upload

---
+++ transferobjectpattern/Application.java
@@ -0,0 +1,19 @@
+package com.java.nyx.transferobjectpattern;
+
+import java.util.Scanner;
+import java.util.UUID;
+
+/**
+ * @Author Nie Yixin
+ * @Description:
+ * @Date: Created in 15:04 2020/11/13
+ * @Modified By:
+ **/
+public class Application {
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.transferObjectPattern();
+
+    }
+}
