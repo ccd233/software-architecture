a2492f0830185312b731d0b41b2b1e63dc84eb35
tongjinyx <43092694+tongjinyx@users.noreply.github.com>
2020-11-17 21:31:09 +0800

    Add files via upload

---
+++ builderpattern/Application.java
@@ -0,0 +1,15 @@
+package com.java.nyx.buildpattern;
+
+import java.util.Scanner;
+/**
+ * @Author Nie Yixin
+ * @Description:
+ * @Date: Created in 13:35 2020/11/13
+ * @Modified By:
+ **/
+public class Application {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.buildPattern();
+    }
+}
